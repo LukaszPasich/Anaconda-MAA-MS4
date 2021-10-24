4c438e4982ca81d280563e316db70b180ad195cc
LukaszPasich <lukas.zed81@gmail.com>
2021-10-24 14:17:55 +0000

    FEAT: Add Memberships view and template

--- membership/views.py
+++ membership/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Membership
 
 # Create your views here.
+
+
+def all_memberships(request):
+    """ A view to show all memberships"""
+
+    memberships = Membership.objects.all()
+
+    context = {
+        'memberships': memberships,
+    }
+
+    return render(request, 'membership/memberships.html', context)
