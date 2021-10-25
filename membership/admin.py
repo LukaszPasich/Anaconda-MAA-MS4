beb6a0ca5b70b596b5a6864ae11389d383f2d2ed
LukaszPasich <lukas.zed81@gmail.com>
2021-10-25 11:27:08 +0000

    FEAT: Modify Membership model, add Discipline model

--- membership/admin.py
+++ membership/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Membership
+from .models import Membership, Discipline
 
+
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
 
 class MembershipAdmin(admin.ModelAdmin):
     list_display = (
         'membership_no',
         'name',
         'small_name',
+        'all_classes',
         'price',
         'pay_interval',
     )
@@ -14,4 +21,5 @@ class MembershipAdmin(admin.ModelAdmin):
     ordering = ('membership_no',)
 
 
+admin.site.register(Discipline, DisciplineAdmin)
 admin.site.register(Membership, MembershipAdmin)
--- membership/models.py
+++ membership/models.py
@@ -2,13 +2,27 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 
+class Discipline(models.Model):
+    """ Model for Disciplines"""
+
+    name = models.CharField(max_length=32)
+    friendly_name = models.CharField(max_length=32, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Membership(models.Model):
     """ Model for Memberships"""
 
+    discipline = models.ForeignKey('Discipline', null=True, blank=True, on_delete=models.SET_NULL)
     membership_no = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=32)
     small_name = models.CharField(max_length=32, null=True, blank=True)
-    description = models.TextField()
+    all_classes = models.BooleanField(default=False)
     price = models.PositiveIntegerField(validators=[MaxValueValidator(999)])
     pay_interval = models.CharField(max_length=32, null=True, blank=True)
 
