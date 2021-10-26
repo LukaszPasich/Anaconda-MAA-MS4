a2a7811132e3f95d8488b687e8098c25733636a7
LukaszPasich <lukas.zed81@gmail.com>
2021-10-26 17:53:39 +0000

    MAINT: Remove secret key and set debug

--- anaconda/settings.py
+++ anaconda/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%exgc^jiri0%$55czu&_!3jd!wk=fp*z$=06$(t0w6w=12t+-@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['anacondamaa.herokuapp.com', 'localhost']
 
