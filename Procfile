eed11737de220f0efde9ee4073bc31e7763be11c
LukaszPasich <lukas.zed81@gmail.com>
2021-10-26 17:04:39 +0000

    MAINT: Heroku deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn anaconda.wsgi:application
--- anaconda/settings.py
+++ anaconda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%exgc^jiri0%$55czu&_!3jd!wk=fp*z$=06$(t0w6w=12t+-@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anacondamaa.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,12 +107,17 @@ WSGI_APPLICATION = 'anaconda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
